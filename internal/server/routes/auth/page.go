894bc58cc57ee7887d135b64ca4b5c58c3fbaffe
Daniil <ksusonic@yandex.ru>
2025-01-07 22:19:35 +0300

    bug: fix tg-callback path

--- internal/server/routes/auth/page.go
+++ internal/server/routes/auth/page.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const tgCallbackPath = "/auth/callback"
+const tgCallbackPath = "/auth/tg-callback"
 
 func (ctrl *Controller) Page(c *gin.Context) {
 	c.Header("Content-Security-Policy", "default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://telegram.org; frame-ancestors 'self' https://oauth.telegram.org;") //nolint:lll
